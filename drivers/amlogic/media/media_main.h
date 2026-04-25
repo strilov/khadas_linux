8254c21c502395cf1fcd78910100782677f82829
Stoyan Trilov <stoyantrilov@duck.com>
2026-04-25 12:42:27 +0100

    earc cec fixes

--- drivers/amlogic/media/media_main.h
+++ drivers/amlogic/media/media_main.h
@@ -523,7 +523,7 @@ static inline int dim_module_init(void)
 }
 #endif
 
-#ifdef CONFIG_AMLOGIC_AO_CEC
+#if defined(CONFIG_AMLOGIC_AO_CEC) || defined(CONFIG_AMLOGIC_LINUX_STD_CEC)
 int cec_init(void);
 #else
 static int cec_init(void)
